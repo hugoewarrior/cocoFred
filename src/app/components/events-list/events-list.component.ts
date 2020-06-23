54553c4130cac34912e90a1ded614451deebc502
hugoewarrior <hugoewarrior@gmail.com>
2020-06-22 21:58:00 -0500

    Apollo server setted up

--- src/app/components/events-list/events-list.component.ts
+++ src/app/components/events-list/events-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Subscription } from 'rxjs';
+import { EVENTS } from '../../../apollo-server/queries/events';
+
 
 @Component({
   selector: 'app-events-list',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsListComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean;
+  events: any;
+  storage: any; 
 
-  ngOnInit(): void {
+  private querySubscription: Subscription;
+
+  constructor(private apollo: Apollo) {
+
+
+   }
+
+  ngOnInit() {
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: EVENTS
+    })
+      .valueChanges
+      .subscribe(({ data, loading }) => {
+        console.log(loading, data, "Loading")
+        this.events = data.events
+      });
+
   }
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
+  }
+
+//   getUrl = (event:any) => {
+//     if (event.pic1) {
+//         this.storage.ref('/events/commonUserEvents/' + event.pic1).getDownloadURL().then((url) => {
+//             return(url)
+//         })
+//     }
+// }
 
 }
